9a612254e68f62dcdbc279caddedb5cdc45f45d6
Colin Thomson <ColinThomson@MineGeoTech.local>
2021-06-18 12:11:13 +0100

    Added Functionality

--- Inventory_Manager.py
+++ Inventory_Manager.py
@@ -23,11 +23,50 @@ class Main(QMainWindow, FORM_CLASS):
         self.Handle_Buttons()
         
     def Handle_Buttons(self):
-        pass
+        self.refresh_button.clicked.connect(self.GET_DATA)
+        self.search_button.clicked.connect(self.SEARCH)
+        
+    def GET_DATA(self):
+        
+        # Connect to sqlite database and fill GUI table with data
+        db=sqlite3.connect("parts.db")
+        cursor=db.cursor()
+        
+        command=''' SELECT * from parts_table '''
+        
+        result=cursor.execute(command)
+        
+        self.table.setRowCount(0)
+        
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+                
+        # Display references number and type number in Statistics tab
+        
+        
+        # Display 4 results: Min, Max Nbr of holes in addition to their respective reference names
     
     
-    # Here is our code
-    
+    def SEARCH(self):
+        
+        # Connect to sqlite database and fill GUI table with data
+        db=sqlite3.connect("parts.db")
+        cursor=db.cursor()
+        
+        nbr=int(self.count_filter_txt.text())
+        
+        command=''' SELECT * from parts_table WHERE count<=?'''
+        
+        result=cursor.execute(command, [nbr])
+        
+        self.table.setRowCount(0)
+        
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
     
 
 
